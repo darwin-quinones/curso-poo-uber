26e30afb27fa2a4348be08e670d8630d396c7a8d
darwin-quinones <darwinquinones735@gmail.com>
2022-09-03 11:58:05 -0500

    Se hace el ejemplo de polimorfismo en php

---
+++ PHP/index.php
@@ -0,0 +1,43 @@
+<?php
+
+// clase de polimorfismo: esto significa diferentes formas  
+//(si se comporta de diferentes maneras y otorga diferentes resultados)
+abstract class Base{
+    protected $name;
+
+    private function getClassName(){
+        return get_called_class();
+    }
+
+    public function login(){
+        return "Mi nombre es $this->name desde la clase {$this->getClassName()}";
+    }
+
+
+}
+
+class Admin extends Base{
+    public function __construct($name){
+        $this->name = $name;
+    }
+}
+
+class User extends Base {
+    public function __construct($name){
+        $this->name = $name;
+    }
+}
+
+class Guest extends Base{
+    protected $name = "Invitado";
+}
+
+// instancias
+$guest = new Guest();
+echo $guest->login();
+
+$admin = new Admin("Darwin");
+echo $admin->login();
+
+$user = new User("Pepe");
+echo $user->login();
